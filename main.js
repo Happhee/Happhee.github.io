7cd85d72aea150d4ffa7b0507dec6ff38086a259
happhee <ghdtj7@naver.com>
2023-12-15 21:22:11 +0900

    feat : 타이핑 애니메이션 구현

---
+++ main.js
@@ -0,0 +1,23 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+const PURPLE_CONTETN = "선한 영향을 주는".split("");
+const MAIN_TITLE =
+  "안녕하세요.\n 선한 영향을 주는 \n 프론트엔드 개발자 홍서희입니다.";
+const main_title = $(".main_title");
+let idx = 0;
+
+const typing = () => {
+  let main_title_txt = MAIN_TITLE[idx++];
+  main_title.innerHTML +=
+    (main_title_txt === "\n" && "<br/>") ||
+    (PURPLE_CONTETN.includes(main_title_txt) &&
+      `<span class='purple'>${main_title_txt}<span>`) ||
+    main_title_txt;
+
+  if (idx > MAIN_TITLE.length) {
+    main_title.textContent = "";
+    idx = 0;
+  }
+};
+setInterval(typing, 200);
